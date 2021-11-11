ca24de70593935952aebf142cf7919e0dc8bac90
Hasan Lam <hcb.lam@gmail.com>
2021-11-11 17:01:34 +0000

    Add HTTP_basic_authenticate for admin access

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_AUTHENTICATE_NAME'], password: ENV['BASIC_AUTHENTICATE_PASSWORD']
+
   def show
   end
 end
