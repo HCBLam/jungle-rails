5ed03bd47dfd70f40a6c9a8979f941fc941d1521
Hasan Lam <hcb.lam@gmail.com>
2021-11-17 02:13:11 +0000

    Add rspec test for email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,35 +3,29 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validation' do
     it 'is not valid if there is no first name' do
-      @user = User.new(first_name: nil, last_name: 'Last', email: 'test@test.com', password_digest: 'secret')
+      @user = User.new(first_name: nil, last_name: 'Last', email: 'test@test.com', password: 'secret1', password_confirmation: 'secret1')
       expect(@user).to_not be_valid
     end
     it 'is not valid if there is no last name' do
-      @user = User.new(first_name: 'First', last_name: nil, email: 'test@test.com', password_digest: 'secret')
+      @user = User.new(first_name: 'First', last_name: nil, email: 'test@test.com', password: 'secret1', password_confirmation: 'secret1')
       expect(@user).to_not be_valid
     end
     it 'is not valid if there is no email' do
-      @user = User.new(first_name: 'First', last_name: 'Last', email: nil, password_digest: 'secret')
+      @user = User.new(first_name: 'First', last_name: 'Last', email: nil, password: 'secret1', password_confirmation: 'secret1')
       expect(@user).to_not be_valid
     end
 
-
-
-    # it 'is not valid if email is not unique and already exists in the database' do
-    # end
-
-    # it { should validate_uniqueness_of(:email).case_sensitive }
-
-    # it 'is not valid if email is not unique' do
-    #   @user1 = User.new(first_name: 'First1', last_name: 'Last1', email: 'test@test.com', password_digest: 'secret1')
-    #   @user2 = User.new(first_name: 'First2', last_name: 'Last2', email: 'test@test.com', password_digest: 'secret2')
-    #   expect(@user2).to_not be_valid
-    # end
-
-
+    it 'is not valid if email is not unique' do
+      user1 = User.new(first_name: 'First1', last_name: 'Last1', email: 'test@test.com', password: 'secret1', password_confirmation: 'secret1')
+      user1.save
+      user2 = User.new(first_name: 'First2', last_name: 'Last2', email: 'test@test.com', password: 'secret2', password_confirmation: 'secret2')
+      user2.save
+      expect(user2).to_not be_valid
+    end
 
     it 'is not valid if there is no password' do
-      @user = User.new(first_name: 'First', last_name: 'Last', email: 'test@test.com', password_digest: nil)
+      @user = User.new(first_name: 'First', last_name: 'Last', email: 'test@test.com')
+      @user.save
       expect(@user).to_not be_valid
     end
 
@@ -41,7 +35,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'is not valid if password is less than minimum length' do
-      @user = User.new(first_name: 'First', last_name: 'Last', email: 'test@test.com', password_digest: 'four')
+      @user = User.new(first_name: 'First', last_name: 'Last', email: 'test@test.com', password: 'secret1', password_confirmation: 'secret1')
       expect(@user).to_not be_valid
     end
 
